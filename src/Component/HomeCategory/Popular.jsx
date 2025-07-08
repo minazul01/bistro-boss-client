08e184467118ef9dfedfa1184c0f487d7c98fe58
minazul01 <minajul4010@gmail.com>
2025-07-09 01:08:46 +0600

    again recap before commit

--- src/Component/HomeCategory/Popular.jsx
+++ src/Component/HomeCategory/Popular.jsx
@@ -21,7 +21,7 @@ const Popular = () => {
 
                 </Title>
             </div>
-            <div className="grid grid-cols-2 gap-5">
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
                 {
                     data.map(item => <MenuItem key={item._id} item={item}></MenuItem>)
                 }
--- src/Component/ShareTitle/MenuItem.jsx
+++ src/Component/ShareTitle/MenuItem.jsx
@@ -1,17 +1,32 @@
+const MenuItem = ({ item }) => {
+  const { name, image, price, recipe } = item;
+  return (
+    <div className="text-black flex flex-col md:flex-row items-center justify-between gap-4 md:gap-5 mb-10 md:mb-20">
+      {/* Image Section */}
+      <div className="w-[150px] shrink-0">
+        <img
+          className="w-full"
+          style={{ borderRadius: "0px 200px 200px 200px" }}
+          src={image}
+          alt="item"
+        />
+      </div>
 
+      {/* Text Section */}
+      <div className="flex-1 text-center md:text-left">
+        <h2 className="text-xl font-medium uppercase">
+          {name}{" "}
+          <span className="hidden md:inline-block">
+            ------------------------
+          </span>
+        </h2>
+        <p className="text-lg font-normal text-gray-500">{recipe}</p>
+      </div>
 
-const MenuItem = ({item}) => {
-    const {name, image, price, recipe} = item;
-    return (
-        <div className="text-black flex flex-row items-center gap-5 mb-20">
-            <div className="w-[150px] "><img style={{borderRadius: '0px 200px 200px 200px'}} src={image} alt="item" /></div>
-            <div>
-                <h2 className="text-xl font-medium uppercase">{name} ------------------------</h2>
-                <p className="text-lg font-normal text-gray-500">{recipe}</p>
-            </div>
-            <p className="text-yellow-400">${price}</p>
-        </div>
-    );
+      {/* Price Section */}
+      <p className="text-yellow-400 text-lg font-semibold">${price}</p>
+    </div>
+  );
 };
 
 export default MenuItem;
